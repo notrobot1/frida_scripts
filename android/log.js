e679d2e8476c2719130e12c7e696957893202d40
NotRobot <64023996+notrobot1@users.noreply.github.com>
2024-03-28 04:56:32 -0700

    Create log.js

---
+++ android/log.js
@@ -0,0 +1,33 @@
+Java.perform(function () {
+   var Log = Java.use("android.util.Log");
+   Log.d.overload('java.lang.String', 'java.lang.String').implementation = function (a, b) {
+       send("Log.d()");
+       send(a.toString());
+       send(b.toString());
+       return this.d.overload('java.lang.String', 'java.lang.String').call(this, a, b);
+   };
+   Log.v.overload("java.lang.String", "java.lang.String").implementation = function (a, b) {
+       send("Log.v()");
+       send(a.toString());
+       send(b.toString());
+       return this.v.overload("java.lang.String", "java.lang.String").call(this, a, b);
+   };
+   Log.i.overload("java.lang.String", "java.lang.String").implementation = function (a, b) {
+       send("Log.i()");
+       send(a.toString());
+       send(b.toString());
+       return this.i.overload("java.lang.String", "java.lang.String").call(this, a, b);
+   };
+   Log.e.overload("java.lang.String", "java.lang.string").implementation = function (a, b) {
+       send("Log.e()");
+       send(a.toString());
+       send(b.toString());
+       return this.e.overload("java.lang.String", "java.lang.String").call(this, a, b);
+   };
+   Log.w.overload("java.lang.String", "java.lang.String").implementation = function (a, b) {
+       send("Log.w()");
+       send(w.toString());
+       send(w.toString());
+       return this.w.overload("java.lang.String", "java.lang.String").call(this, a, b);
+   };
+});
